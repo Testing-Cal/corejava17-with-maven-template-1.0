Testing-Cal/corejava21-with-maven-template

Upgrade to tomcat 10.1.5

Dockerfile
@@ -2,6 +2,6 @@ FROM amazoncorretto:21-alpine
 ENV context ""
 ENV port 8182
 ADD /src/main/resources/application.properties //
-ADD /target/corejava21-with-maven-template-1.0-SNAPSHOT.one-jar.jar //
+ADD /target/corejava21-with-maven-template-1.0-SNAPSHOT-jar-with-dependencies.jar //
 ENTRYPOINT ["java","-jar", "/corejava21-with-maven-template-1.0-SNAPSHOT-jar-with-dependencies.jar","--server.port=${port}"]
 

Testing-Cal/corejava17-with-maven-template-1.0

Dockerfile
@@ -2,6 +2,6 @@ FROM amazoncorretto:17-alpine
 ENV context ""
 ENV port 8022
 ADD /src/main/resources/application.properties //
-ADD /target/corejava17-with-maven-template-1.0-SNAPSHOT.one-jar.jar //
+ADD /target/corejava17-with-maven-template-1.0-SNAPSHOT-jar-with-dependencies.jar //
 ENTRYPOINT ["java","-jar", "/corejava17-with-maven-template-1.0-SNAPSHOT-jar-with-dependencies.jar","--server.port=${port}"]
 